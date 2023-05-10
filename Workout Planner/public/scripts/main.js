3f3a06eb6ff11463001d59be73ef110b378a862d
rhit-huangr2 <huangr2@rose-hulman.edu>
2023-05-10 00:05:34 -0400

    more html, css, firebase and deletes

--- Workout Planner/public/scripts/main.js
+++ Workout Planner/public/scripts/main.js
@@ -43,14 +43,14 @@ rhit.MyPlansManager = class {
     this._ref = firebase.firestore().collection("Workout Plans");
     this._unsubscribe = null;
   }
-  add(name, goal, diff, days, favorite, uid, time, exercises) {
+  add(name, goal, diff, days, favorite, time, exercises) {
     this._ref.add({
       ["Name"]: name,
       ["Goal"]: goal,
       ["Difficulty"]: diff,
       ["Days"]: days,
       ["favorite"]: favorite,
-      ["uid"]: uid,
+      ["uid"]: rhit.fbAuthManager.uid,
       ["time"]: time,
       ["favorite"]: favorite,
       ["Weekday"]: exercises,
@@ -80,8 +80,9 @@ rhit.MyPlansManager = class {
   update() {
     //TODO: WRITE EDIT PLAN METHOD
   }
-  delete(id) {
+  delete() {
     //TODO: WRITE DELETE PLAN METHOD
+    // return this._ref.delete();
   }
   get length() {
     return this._documentSnapshots.length;
@@ -119,7 +120,7 @@ rhit.ExistingPlansManager = class {
   stopListening() {
     this._unsubscribe();
   }
-  
+
   get length() {
     return this._documentSnapshots.length;
   }
@@ -178,6 +179,65 @@ rhit.FBAuthManager = class {
   }
 };
 
+rhit.SinglePlanManager = class {
+	constructor(planId) {
+		this._documentSnapshot = {};
+		this._unsubscribe = null;
+		this._ref = firebase.firestore().collection("Workout Plans").doc(planId);
+	}
+	beginListening(changeListener) {
+
+		this._unsubscribe = this._ref.onSnapshot((doc) => {
+			if (doc.exists) {
+				console.log("Document data: ", doc.data());
+				this._documentSnapshot = doc;
+				changeListener();
+			} else {
+				console.log("No such document!");
+				// window.location.href = "/"'
+			}
+		});
+
+	}
+	stopListening() {
+		this._unsubscribe();
+	}
+
+	update(name, goal, diff, days, favorite, time, exercises) {
+		// console.log("update quote movie");
+		this._ref.update({
+			["Name"]: name,
+      ["Goal"]: goal,
+      ["Difficulty"]: diff,
+      ["Days"]: days,
+      ["favorite"]: favorite,
+      ["uid"]: rhit.fbAuthManager.uid,
+      ["time"]: time,
+      ["favorite"]: favorite,
+      ["Weekday"]: exercises,
+		})
+			.then(() => {
+				console.log("document successfulle updated");
+			})
+			.catch(function (error) {
+				console.log("Error adding document: ", error);
+			});
+	}
+
+	delete(){
+		return this._ref.delete();
+		
+	}
+
+	get name() {
+		return this._documentSnapshot.get("Name");
+	}
+
+	get goal() {
+		return this._documentSnapshot.get("Goal");
+	}
+}
+
 
 
 //
@@ -191,6 +251,16 @@ rhit.MyPlansController = class {
     document.querySelector("#existingButton").onclick = (event) => {
       window.location.href = "/existingPlans.html";
     };
+    // document.querySelector("#submitDeletePlan").addEventListener("click", (event) => {
+    //   rhit.myPlansManager.delete().then(function () {
+    //     console.log("document deleted");
+    //     // window.location.href = "/list.html";
+    //   }).catch(function (error) {
+    //     console.log("error removing doc", error);
+    //   });;
+// 
+    // });
+
 
     // will come back to this
     //   $("#addCustomDialog").on("show.bs.modal", (event) => {
@@ -206,26 +276,13 @@ rhit.MyPlansController = class {
     rhit.myPlansManager.beginListening(this.updateList.bind(this));
   }
   _createCard(wp) {
-    console.log(wp.name);
-    //brennan's code
-    //     return htmlToElement(
-    //       ` <div style="width: 18rem;">
-    //       <div class="card-body">
-    //         <h5 class="card-title">${wp.name}</h5>
-    //         <a href="#" class="card-link">Edit</a>
-    //         <a href="#" class="card-link">Favorite</a>
-    //         <a href="#" class="card-link">Delete</a>
-    //       </div>
-    //     </div>`);
-
     return htmlToElement(
       ` <div style="width: 18rem;">
       <div class="card-body">
         <h5 class="card-title">${wp.name}</h5>
-        <a href="#" class="card-link">Edit</a>
-        <span>Favorite</span>
+        <lab>
         <input type="radio" id="favorite" name="fav_plan" value="${wp.name}">
-        <a href="#" class="card-link">Delete</a>
+        <label for="favorite">Favorite</label><br>
       </div>
     </div>`
     );
@@ -238,17 +295,21 @@ rhit.MyPlansController = class {
     for (let i = 0; i < rhit.myPlansManager.length; i++) {
       const wp = rhit.myPlansManager.getPlanAtIndex(i);
       console.log("wp.uid: " + wp.uid + " auth id: " + rhit.fbAuthManager.uid);
-      if (wp.uid == rhit.fbAuthManager.uid) {
-        const newCard = this._createCard(wp);
-
-        //TODO: ADD LISTENERS FOR EDIT FAVORITE AND DELETE BUTTONS
+      const newCard = this._createCard(wp);
+      
+      newCard.onclick = (event) => {
+        
+        window.location.href = `/plan.html?id=${wp.id}`;
+      };
+      //TODO: ADD LISTENERS FOR EDIT FAVORITE AND DELETE BUTTONS
+      // if (wp.uid == rhit.fbAuthManager.uid) {
 
         // newCard.onclick = (event) => {
         // 	window.location.href = `/moviequote.html?id=${mq.id}`;
         // }
 
         newList.appendChild(newCard);
-      }
+      // }
     }
 
     const oldList = document.querySelector("#plansList");
@@ -259,8 +320,50 @@ rhit.MyPlansController = class {
   }
 };
 
+rhit.SinglePlanController = class {
+	constructor() {
+		document.querySelector("#submitEditPlan").addEventListener("click", (event) => {
+			const quote = document.querySelector("#inputQuote").value;
+			const movie = document.querySelector("#inputMovie").value;
+			// rhit.fbSingleQuoteManager.update(quote, movie);
+		});
+		$("#editPlanDialog").on("show.bs.modal", (event) => {
+			//pre 
+      // document.querySelector("#inputQuote").value = rhit.fbSingleQuoteManager.quote;
+			// document.querySelector("#inputMovie").value = rhit.fbSingleQuoteManager.movie;
+			document.querySelector("#inputQuote").value = "some plan";
+			document.querySelector("#inputMovie").value = "info";
+		});
+		$("#editPlanDialog").on("shown.bs.modal", (event) => {
+		// 	//post animation
+			document.querySelector("#inputQuote").focus();
+		});
+
+
+		document.querySelector("#submitDeletePlan").addEventListener("click", (event) => {
+			rhit.singlePlanManager.delete().then(function(){
+				// console.log("document deleted");
+				window.location.href = "/myPlans.html";
+			}).catch(function(error){
+				console.log("error removing doc", error);
+			});;
+
+		});
+
+
+		rhit.singlePlanManager.beginListening(this.updateView.bind(this));
+	}
+	updateView() {
+		// document.querySelector("#cardQuote").innerHTML = rhit.fbSingleQuoteManager.quote;
+		// document.querySelector("#cardMovie").innerHTML = rhit.fbSingleQuoteManager.movie;
+    document.querySelector("#cardQuote").innerHTML = "aaaaaaaa";
+		document.querySelector("#cardMovie").innerHTML = "bbbbbb";
+	}
+}
+
+
 rhit.TodaysWorkoutController = class {
-  constructor() {}
+  constructor() { }
 };
 
 rhit.MyAccountController = class {
@@ -273,7 +376,25 @@ rhit.MyAccountController = class {
 };
 
 rhit.CustomPlanController = class {
-  constructor() {}
+  constructor() {
+    const Weekday = {
+      Monday: {},
+      Tuesday: {},
+      Wednesday: {},
+      Thursday: {},
+      Friday: {},
+      Saturday: {},
+      Sunday: {},
+    }
+    document.querySelector("#customBack").onclick = (event) => {
+      window.location.href = '/myPlans.html';
+    }
+    document.querySelector("#customSave").onclick = (event) => {
+      // name, goal, diff, days, favorite time, exercises
+      rhit.myPlansManager.add("1", "1", "1", "1", false, "60", Weekday);
+      alert("Saved");
+    }
+  }
 };
 
 rhit.HomePageController = class {
@@ -370,7 +491,7 @@ rhit.ExistingPlansController = class {
       const wp = rhit.existingPlansManager.getPlanAtIndex(i);
       if (wp.uid == "") {
         const newCard = this._createCard(wp);
-        
+
         newCard.onclick = (event) => {
           rhit.myPlansManager.addExisting(wp);
         };
@@ -415,6 +536,18 @@ rhit.main = function () {
   if (document.querySelector("#existingPage")) {
     this.existingPlansManager = new rhit.ExistingPlansManager();
     new rhit.ExistingPlansController();
+  }
+  if (document.querySelector("#planPage")) {
+    const queryString = window.location.search;
+		console.log(queryString);
+		const urlParams = new URLSearchParams(queryString);
+		const planId = urlParams.get("id");
+
+		if (!planId) {
+			window.location.href = "/";
+		}
+    rhit.singlePlanManager = new rhit.SinglePlanManager(planId);
+    new rhit.SinglePlanController();
   }
 };
 
